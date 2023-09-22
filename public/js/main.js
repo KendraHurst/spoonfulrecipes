bc058e54fac83c147f35cbc7acd840740303ea27
Kendra Hurst <kehu7991@gmail.com>
2023-09-22 08:35:33 -0600

    Delay ads load

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,14 @@
 function loadAds() {
-	const head = document.querySelector('head');
-	const adsScript = document.createElement('script');
+	setTimeout(() => {
+		const head = document.querySelector('head');
+		const adsScript = document.createElement('script');
 
-	adsScript.setAttribute('async', '');
-	adsScript.setAttribute('crossorigin', 'anonymous');
-	adsScript.setAttribute('src', 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6117958085618253');
+		adsScript.setAttribute('async', '');
+		adsScript.setAttribute('crossorigin', 'anonymous');
+		adsScript.setAttribute('src', 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6117958085618253');
 
-	head.appendChild(adsScript);
+		head.appendChild(adsScript);
+	}, 5000);
 }
 
 window.onload = loadAds;
