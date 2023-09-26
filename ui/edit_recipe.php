605460fc8c6a139da75a4cfbbb2f9a50ef0f898c
Kendra Hurst <kehu7991@gmail.com>
2023-09-25 20:48:19 -0600

    Minor fix

--- ui/edit_recipe.php
+++ ui/edit_recipe.php
@@ -10,8 +10,6 @@ $categories = explode(', ', $recipe['category']);
 echo $view->render('inc/head.php', null, compact('f3', 'view', 'page'));
 echo $view->render('inc/header.php', null, compact('f3', 'view', 'page'));
 	?>
-<script src="https://cdn.ckeditor.com/ckeditor5/38.1.0/classic/ckeditor.js"></script>
-<script src="js/new_recipe.js"></script>
 <style>
 	#directions-list .btn, #directions-list input {
 		line-height: 48px;
@@ -243,6 +241,8 @@ echo $view->render('inc/header.php', null, compact('f3', 'view', 'page'));
 	</form>
 </div>
 
+<script src="js/ckeditor.js"></script>
+<script src="js/new_recipe.js"></script>
 <?php
 echo $view->render('inc/footer.php', null, compact('f3', 'view', 'page'));
 ?>
